4f9f12fd7f91df100968a68744784d523e00b800
Danil <fomka.homca@gmail.com>
2025-09-30 11:51:01 +0300

    Add files via upload
    hotfix 1

--- ToDo_project.py
+++ ToDo_project.py
@@ -40,7 +40,7 @@ class ToDoList:
         '''блок создания нового порядкового айди задачи'''
 
         self.cur.execute(f"""INSERT INTO task_list (id, name, text, deadline, status, importance, user_id)
-                         VALUES ({new_id}, '{name}', '{text}', '{deadline}', '{status}', '{importance}', {user_id}) )""")
+                         VALUES({new_id}, '{name}', '{text}', '{deadline}', '{status}', '{importance}', {user_id})""")
         
         self.connect.commit()
         
@@ -48,8 +48,8 @@ class ToDoList:
     def read(self, user_id: int):
         '''метод класса для вывода задач по определенному user_id. для вывода всех задач введите 0 id'''
 
-        if (id == 0):
-            id = '*'
+        if (user_id == 0):
+            user_id = '*'
 
         self.cur.execute(f"""SELECT * FROM task_list WHERE user_id = {user_id}""")
         tasks = self.cur.fetchall()
@@ -79,18 +79,20 @@ class ToDoList:
 
         self.connect.commit()
 
+        print('Задача удалена')
+
 
 
 class User:
-    def __init__(self,file_name: str, user_name: str, password: int):
+    def __init__(self,file_name: str):
         '''Класс пользователя с уникальным порядковым айди и именем и почти личным паролем'''
-
-        self.user_name = user_name
-        self.password = password
 
         self.file_name = file_name
         self.connect = sqlite3.connect(f"{self.file_name}.db")
         self.cur = self.connect.cursor()
+
+
+    def create_user(self, user_name: str, password: str):
 
         self.cur.execute("""SELECT id FROM users""")
         id_list = self.cur.fetchall()
@@ -99,28 +101,30 @@ class User:
 
         self.cur.execute("""SELECT name FROM users""")
         name_list = self.cur.fetchall()
-        
+
         while (True):
 
-            for name in name_list:
+            for names in name_list:
 
-                if (name == self.user_name):
+                for name in names:
 
-                    self.user_name = input('имя пользователя существует, введите другое')
+                    if (name == user_name):
+
+                        user_name = input('имя пользователя существует, введите другое: ')
 
             break
-        '''проверка существующих  имен'''
+        '''проверка существующих  имен. 2 цикла for так как получаем массив из списков и их тоже надо открыть'''
 
-        self.cur.execute(f"""INSERT INTO users (id, name, password) VALUES ({new_id}, '{self.user_name}', '{self.password}')""")
+        self.cur.execute(f"""INSERT INTO users (id, name, password) VALUES ({new_id}, '{user_name}', '{password}')""")
 
         self.connect.commit()
-
+    
 
     def check_id(self, name: str):
         '''проверка своего айди по имени пользователя'''
         
         self.cur.execute(f"""SELECT id FROM users WHERE name = '{name}'""")
-        id = self.cur.fetchall
+        id = self.cur.fetchall()
 
         print(id)
 
@@ -130,21 +134,34 @@ class User:
 
 
 
-# FirstList = ToDoList('first_list')
-# Danil = User('first_list', 'Danil', 123)
-# Danil.check_id('Danil')
+FirstList = ToDoList('first_list')
 
+# cjplfybt gjkmpjdfntkz
+Danil = User('first_list')
+# Danil.create_user('Danil', '123')
+Danil.check_id('Danil')
 
-# FirstList.create('задача 1', 'тест', 'сегодня', 'выдана', 'важная', )
+# коннект для работы с бд
+connect = sqlite3.connect(f"first_list.db")
+cur = connect.cursor()
 
+# вывод списка имен
+# cur.execute("""SELECT name FROM users """)
+# names = cur.fetchall()
+# for name in names:
+#     for user_name in name:
+#         print (user_name)
 
+# проверка указания айди задачи
+# cur.execute("""SELECT id FROM task_list""")
+# id_list = cur.fetchall()
+# new_id = len(id_list) + 1
+# print(id_list)
+# print(new_id)
 
-    
+# создание, проверка и удаление задачи
+FirstList.create('задача 1', 'тест', 'сегодня', 'выдана', 'важная', 1)
 
+FirstList.read(1)
 
-
-
-
-
-
-
+FirstList.delete(1)
